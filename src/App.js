3e89a68879a23a0fb574e7477262b28f7f282fff
Raman Saini <120019686+saini-saini@users.noreply.github.com>
2024-05-23 00:07:12 +0530

    makes private and public routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Layout from "./layout/layout";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import Note from "./note/note";
 import Password from "./password/password";
 import "./App.css";
@@ -7,8 +7,24 @@ import SignUp from "./signUp/signUp";
 import Login from "./login/login";
 import {auth} from "./firebaseFireStore/config";
 import { useEffect, useState } from "react";
+import PageNotFound from "./pageNoteFound/pageNotFound";
+
 function App() {
 const [userName, setUserName] = useState("");
+
+const [isAuthenticated, setIsAuthenticated] = useState(false);
+function PrivateRoute({ isAuthenticated }) {
+  if (!isAuthenticated) return <Navigate to="/" />;
+  return <Outlet />;
+}
+
+function PublicRoute({ isAuthenticated }) {
+  if (isAuthenticated) {
+    return <Navigate to="/home" replace />;
+  }
+  return <Outlet />;
+}
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
@@ -22,14 +38,22 @@ const [userName, setUserName] = useState("");
     <BrowserRouter>
       <div className="App">
         <Routes>
+        
+        <Route element={<PublicRoute isAuthenticated={!!userName} />}  >
         <Route path="/" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+          </Route>
 
+          <Route element={<PrivateRoute isAuthenticated={!!userName} />} >
           <Route path="/home" element={<Layout userName={userName}/>}>
             <Route index element={<Note />} />
             <Route path="password" element={<Password />} />
           </Route>
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+
         </Routes>
+        
       </div>
     </BrowserRouter>
   );
